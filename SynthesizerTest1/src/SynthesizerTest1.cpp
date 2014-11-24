3281e491e77dbbefb06dcfa241e6e1ac75a8ac94
Gookie <daniel.fritsch1@gmx.de>
2014-11-24 11:27:45 +0100

    Hello world

--- SynthesizerTest1/src/SynthesizerTest1.cpp
+++ SynthesizerTest1/src/SynthesizerTest1.cpp
@@ -11,5 +11,6 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Hello World :)" << endl;	// prints friendly Hello World :)
 	return 0;
 }
